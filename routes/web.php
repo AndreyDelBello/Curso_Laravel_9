4f68f562d1777c90cae443a426eaefa978a85669
User <user@pop-os.localdomain>
2022-08-01 11:40:27 -0300

    Ultima alteração

--- routes/web.php
+++ routes/web.php
@@ -22,3 +22,19 @@ Route::get('/contato/{id?}', ['uses'=>'App\Http\Controllers\ContatoController@in
 Route::post('/contato', ['uses'=>'App\Http\Controllers\ContatoController@criar']);
 
 Route::put('/contato', ['uses'=>'App\Http\Controllers\ContatoController@editar']);
+
+Route::get('/admin/cursos',['as'=>'admin.cursos','uses'=>'Admin\CursoController@index']);
+Route::get('/admin/cursos/adicionar',['as'=>'admin.cursos.adicionar','uses'=>'Admin\CursoController@adicionar']);
+Route::get('/admin/cursos/salvar',['as'=>'admin.cursos.salvar','uses'=>'Admin\CursoController@salvar']);
+Route::get('/admin/cursos/editar/{id}',['as'=>'admin.cursos.editar','uses'=>'Admin\CursoController@editar']);
+Route::get('/admin/cursos/deletar/{id}',['as'=>'admin.cursos.deletar','uses'=>'Admin\CursoController@deletar']);
+
+
+
+
+
+
+
+
+
+
